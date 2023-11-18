74af615ceddcc782b00a6112def3d710be8852f0
VishaLPatiL9029 <vishalpatil228@gmail.com>
2023-11-18 09:22:12 +0530

    first commit

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     args=argparse.ArgumentParser()
     args.add_argument("--name", "-n", default="sunny", type=str)
     args.add_argument("--age", "-a", default=25.0, type=float)
+    args.add_argument("--job", "-j", default= "Data Scientist", type=str)
     parse_args=args.parse_args()
     
-    print(parse_args.name,parse_args.age)
+    print(parse_args.name,parse_args.age, parse_args.job)
