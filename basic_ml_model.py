3ce59351aea6b76a406075e9bcb29d907c55823d
VishaLPatiL9029 <vishalpatil228@gmail.com>
2023-11-18 14:51:18 +0530

    mlflow update

--- basic_ml_model.py
+++ basic_ml_model.py
@@ -8,6 +8,7 @@ import mlflow.sklearn
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 from sklearn.linear_model import ElasticNet
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
@@ -34,9 +35,9 @@ def evaluate(y_true, y_pred, pred_prob):
     # return mae, mse, rmse, r2
 
     accuracy = accuracy_score(y_true, y_pred)
-    roc_auc_score = roc_auc_score(y_true, pred_prob, multi_class = 'ovr')
+    rc_score = roc_auc_score(y_true, pred_prob, multi_class = 'ovr')
 
-    return accuracy, roc_auc_score
+    return accuracy, rc_score
 
 
 
@@ -58,26 +59,31 @@ def main(n_estimators, max_depth):
 
         # # evaluate the model
         # mae, mse, rmse, r2 = evaluate(y_test, pred)
-
-        accuracy, roc_auc_score = evaluate(y_test, pred, pred_prob)
+ 
+        accuracy, rc_score = evaluate(y_test, pred, pred_prob)
 
         mlflow.log_param("n_estimators", n_estimators)
         mlflow.log_param("max_depth", max_depth)
         mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("accuracy", roc_auc_score)
+        mlflow.log_metric("roc_auc_score", rc_score)
+
+        #mlflow model logging
+        mlflow.sklearn.log_model(rf, "randomforestmodel")
 
         # print(f"mean_absolute_error {mae}, /mean_squared_error{mse}, /root mean squard error {rmse}, accuracy{accuracy}, r2_score {r2}")
 
-        print(f"accuracy {accuracy}")
+        print(f"accuracy {accuracy}, roc_auc_curve {rc_score}")
+
 
 
 if __name__ == '__main__':
-
+    # Corrected the variable name for the argument parser
     args = argparse.ArgumentParser()
-    args.add_argument("--n estimators", "-n", default = 50, type = int)
-    args.add_argument("--max_depth", "-m", default = 5, type = int)
+    args.add_argument("--n_estimators", "-n", default=50, type=int)
+    args.add_argument("--max_depth", "-m", default=5, type=int)
     parse_args = args.parse_args()
+
     try:
-        main(n_estimators = parse_args.n_estimators, max_depth = parse_args.max_depth)
+        main(n_estimators=getattr(parse_args, 'n_estimators', 50), max_depth=getattr(parse_args, 'max_depth', 5))
     except Exception as e:
         raise e
